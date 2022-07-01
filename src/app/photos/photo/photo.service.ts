3c50ba8e13d7a37fc4145cfc2669f7cf61196f56
Daniellyfreitasc <daniellyfreitasc@hotmail.com>
2022-07-01 11:41:25 -0300

    Tipando API

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Photo } from './photo';
 
 
 const API = 'http://localhost:3000';
@@ -16,6 +17,6 @@ export class PhotoService{
     listFromUser(userName: string){
         
         return this.http
-        .get<Object[]>(API + '/flavio/photos');
+        .get<Photo[]>(API + '/flavio/photos');
     }
 }
