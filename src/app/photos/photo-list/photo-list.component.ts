ada77bc4e05207ace1d8cc49d91454f5d74247ac
Daniellyfreitasc <daniellyfreitasc@hotmail.com>
2022-07-04 17:30:32 -0300

    ajustando rotas

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
@@ -12,12 +13,18 @@ export class PhotoListComponent implements OnInit {
   
   
   //nome da variavel "http" e tipagem "HttpClient"
-  constructor(private photoService: PhotoService){ }
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute // indica a rota ativada no momento
+    )
+    { }
     
  ngOnInit(): void {
 
+  const userName =  this.activatedRoute.snapshot.params['userName'];
+
   this.photoService
-    .listFromUser('flavio')
+    .listFromUser(userName)
     .subscribe(photos => this.photos = photos);
     
  }
